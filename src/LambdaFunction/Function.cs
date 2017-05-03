40ca27be36db27014e9ffa3009126ce57c51e6aa
Max Horstmann <max.horstmann@gmail.com>
2017-05-03 16:50:14 -0400

    empty function

---
+++ src/LambdaFunction/Function.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Newtonsoft.Json;
+using Amazon.Lambda.Core;
+using Alexa.NET.Request;
+using Alexa.NET.Response;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace LambdaFunction
+{
+    public class Function
+    {
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            SkillResponse response = new SkillResponse();
+            response.Response = new ResponseBody();
+            response.Response.ShouldEndSession = false;
+            IOutputSpeech innerResponse = null;
+            var log = context.Logger;
+            log.LogLine($"Skill Request Object:");
+            log.LogLine(JsonConvert.SerializeObject(input));
+
+            //var allResources = GetResources();
+            //var resource = allResources.FirstOrDefault();
+
+            //if (input.GetRequestType() == typeof(LaunchRequest))
+            //{
+            //    log.LogLine($"Default LaunchRequest made: 'Alexa, open Science Facts");
+            //    innerResponse = new PlainTextOutputSpeech();
+            //    (innerResponse as PlainTextOutputSpeech).Text = emitNewFact(resource, true);
+
+            //}
+            //else if (input.GetRequestType() == typeof(IntentRequest))
+            //{
+            //    var intentRequest = (IntentRequest)input.Request;
+
+            //    switch (intentRequest.Intent.Name)
+            //    {
+            //        case "AMAZON.CancelIntent":
+            //            log.LogLine($"AMAZON.CancelIntent: send StopMessage");
+            //            innerResponse = new PlainTextOutputSpeech();
+            //            (innerResponse as PlainTextOutputSpeech).Text = resource.StopMessage;
+            //            response.Response.ShouldEndSession = true;
+            //            break;
+            //        case "AMAZON.StopIntent":
+            //            log.LogLine($"AMAZON.StopIntent: send StopMessage");
+            //            innerResponse = new PlainTextOutputSpeech();
+            //            (innerResponse as PlainTextOutputSpeech).Text = resource.StopMessage;
+            //            response.Response.ShouldEndSession = true;
+            //            break;
+            //        case "AMAZON.HelpIntent":
+            //            log.LogLine($"AMAZON.HelpIntent: send HelpMessage");
+            //            innerResponse = new PlainTextOutputSpeech();
+            //            (innerResponse as PlainTextOutputSpeech).Text = resource.HelpMessage;
+            //            break;
+            //        case "GetFactIntent":
+            //            log.LogLine($"GetFactIntent sent: send new fact");
+            //            innerResponse = new PlainTextOutputSpeech();
+            //            (innerResponse as PlainTextOutputSpeech).Text = emitNewFact(resource, false);
+            //            break;
+            //        case "GetNewFactIntent":
+            //            log.LogLine($"GetFactIntent sent: send new fact");
+            //            innerResponse = new PlainTextOutputSpeech();
+            //            (innerResponse as PlainTextOutputSpeech).Text = emitNewFact(resource, false);
+            //            break;
+            //        default:
+            //            log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
+            //            innerResponse = new PlainTextOutputSpeech();
+            //            (innerResponse as PlainTextOutputSpeech).Text = resource.HelpReprompt;
+            //            break;
+            //    }
+            //}
+
+            response.Response.OutputSpeech = innerResponse;
+            response.Version = "1.0";
+            log.LogLine($"Skill Response Object...");
+            log.LogLine(JsonConvert.SerializeObject(response));
+            return response;
+        }
+    }
+}
