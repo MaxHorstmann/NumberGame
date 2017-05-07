4351e8ded2e9333d8845c66963053fdd8098d8dd
Max Horstmann <max.horstmann@gmail.com>
2017-05-06 20:52:11 -0400

    play with session state

--- src/LambdaFunction/Function.cs
+++ src/LambdaFunction/Function.cs
@@ -10,8 +10,20 @@ using Alexa.NET.Request.Type;
 
 namespace LambdaFunction
 {
+    class NumberFact
+    {
+        public string Question { get; set; }
+        public int Answer { get; set; }
+    }
+
     public class Function
     {
+        NumberFact[] numberFacts = new[] 
+        {
+            new NumberFact() { Question = "What is the population of New Zealand?", Answer = 4795070 },
+            new NumberFact() { Question = "What year was Beyoncé born?", Answer = 1981 }
+        };
+
         // LambdaFunction::LambdaFunction.Function::FunctionHandler
         public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
         {
@@ -23,6 +35,12 @@ namespace LambdaFunction
             log.LogLine($"Skill Request Object:");
             log.LogLine(JsonConvert.SerializeObject(input));
 
+            var count = 0; 
+            if (input.Session.Attributes.ContainsKey("Foo"))
+            {
+                count = input.Session.Attributes["Foo"] as int? ?? 0;
+            }
+
             if (input.GetRequestType() == typeof(LaunchRequest))
             {
                 log.LogLine($"Default LaunchRequest made: 'Alexa, open numbers game");
@@ -30,52 +48,51 @@ namespace LambdaFunction
                 {
                     Text = "Welcome to the numbers game!"
                 };
-                response.Response.ShouldEndSession = true;
 
+                var launchRequest = input.Request as LaunchRequest;
+                
+                
             }
-            //else if (input.GetRequestType() == typeof(IntentRequest))
-            //{
-            //    var intentRequest = (IntentRequest)input.Request;
+            else if (input.GetRequestType() == typeof(IntentRequest))
+            {
+                var intentRequest = (IntentRequest)input.Request;
 
-            //    switch (intentRequest.Intent.Name)
-            //    {
-            //        case "AMAZON.CancelIntent":
-            //            log.LogLine($"AMAZON.CancelIntent: send StopMessage");
-            //            innerResponse = new PlainTextOutputSpeech();
-            //            (innerResponse as PlainTextOutputSpeech).Text = resource.StopMessage;
-            //            response.Response.ShouldEndSession = true;
-            //            break;
-            //        case "AMAZON.StopIntent":
-            //            log.LogLine($"AMAZON.StopIntent: send StopMessage");
-            //            innerResponse = new PlainTextOutputSpeech();
-            //            (innerResponse as PlainTextOutputSpeech).Text = resource.StopMessage;
-            //            response.Response.ShouldEndSession = true;
-            //            break;
-            //        case "AMAZON.HelpIntent":
-            //            log.LogLine($"AMAZON.HelpIntent: send HelpMessage");
-            //            innerResponse = new PlainTextOutputSpeech();
-            //            (innerResponse as PlainTextOutputSpeech).Text = resource.HelpMessage;
-            //            break;
-            //        case "GetFactIntent":
-            //            log.LogLine($"GetFactIntent sent: send new fact");
-            //            innerResponse = new PlainTextOutputSpeech();
-            //            (innerResponse as PlainTextOutputSpeech).Text = emitNewFact(resource, false);
-            //            break;
-            //        case "GetNewFactIntent":
-            //            log.LogLine($"GetFactIntent sent: send new fact");
-            //            innerResponse = new PlainTextOutputSpeech();
-            //            (innerResponse as PlainTextOutputSpeech).Text = emitNewFact(resource, false);
-            //            break;
-            //        default:
-            //            log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
-            //            innerResponse = new PlainTextOutputSpeech();
-            //            (innerResponse as PlainTextOutputSpeech).Text = resource.HelpReprompt;
-            //            break;
-            //    }
-            //}
+                switch (intentRequest.Intent.Name)
+                {
+                    case "AMAZON.CancelIntent":
+                        log.LogLine($"AMAZON.CancelIntent: send StopMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        //(innerResponse as PlainTextOutputSpeech).Text = resource.StopMessage;
+                        response.Response.ShouldEndSession = true;
+                        break;
+                    case "AMAZON.StopIntent":
+                        log.LogLine($"AMAZON.StopIntent: send StopMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        //(innerResponse as PlainTextOutputSpeech).Text = resource.StopMessage;
+                        response.Response.ShouldEndSession = true;
+                        break;
+                    case "AMAZON.HelpIntent":
+                        log.LogLine($"AMAZON.HelpIntent: send HelpMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        //(innerResponse as PlainTextOutputSpeech).Text = resource.HelpMessage;
+                        break;
+                    case "MoreIntent":
+                        log.LogLine($"More intent");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = $"Count is {count}";
+                        break;
+                    default:
+                        log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
+                        innerResponse = new PlainTextOutputSpeech();
+                        //(innerResponse as PlainTextOutputSpeech).Text = resource.HelpReprompt;
+                        break;
+                }
+            }
 
             response.Response.OutputSpeech = innerResponse;
             response.Version = "1.0";
+            response.SessionAttributes.Add("foo", count++);
+
             log.LogLine($"Skill Response Object...");
             log.LogLine(JsonConvert.SerializeObject(response));
             return response;
